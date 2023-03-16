81269ec89a81451288abbd1ef8521c7c2a94819d
billupati maheswari <bmaheswari474@gmail.com>
2023-03-16 23:25:23 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bindu(request):
+    return render(request,'bindu.html')
+def mahitha(request):
+    return render(request,'mahitha.html')
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def madhusri(request):
+    return render(request,'madhusri.html')
+def srividya(request):
+    return render(request,'srividya.html')
